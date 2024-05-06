1e412cc7239a431113fbb83035ee0a526f68fd65
sangjun <byjun8932@gmail.com>
2024-05-06 19:05:59 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/byjun0302/flutter_leanr_byjun0302/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.byjun0302.flutter_leanr_byjun0302
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/StatefulWidgetSample.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StatefulWidgetSample extends StatefulWidget {
+  const StatefulWidgetSample({super.key});
+
+  @override
+  State<StatefulWidgetSample> createState() => _StatefulWidgetSampleState();
+}
+
+class _StatefulWidgetSampleState extends State<StatefulWidgetSample> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
